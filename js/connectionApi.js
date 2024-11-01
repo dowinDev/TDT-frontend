87b5909f2709288260c2ae4b6d5e25b3b7025a7c
do.thang <100296543+DTD2604@users.noreply.github.com>
2024-11-01 11:02:03 +0700

    fix gitignore

---
+++ js/connectionApi.js
@@ -0,0 +1,21 @@
+const env = 'dev';
+
+export const limitProduct = 8;
+
+let http = '';
+if (env === 'dev') {
+    http = `http://localhost:3000/api`;
+} else {
+    http = `https://be-food-sharing.btecit.tech/api`;
+}
+
+
+export const login = `${http}/accounts/login`;
+export const logOut = `${http}/accounts/logout`;
+export const register = `${http}/accounts/register`;
+export const sendOtp = `${http}/accounts/send-otp`;
+export const verifyOtp = `${http}/accounts/verify-otp`;
+export const users = `${http}/users`;
+export const products = `${http}/products`;
+export const refresh = `${http}/accounts/refresh`;
+export const checkToken = `${http}/accounts/checkToken`;
